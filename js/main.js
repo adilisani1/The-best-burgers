372ec24752be7200aac1f7d34221bbcccddba6f9
Adil Isani <adil.isani1@gmail.com>
2022-08-04 17:16:05 +0500

    footer-completed

--- js/main.js
+++ js/main.js
@@ -10,3 +10,48 @@ $(function () {
 });
 
 
+
+
+$(document).ready(function () {
+    $(".card").click(function () {
+        $(".card").removeClass("active");
+        // $(".tab").addClass("active"); // instead of this do the below 
+        $(this).addClass("active");
+    });
+});
+
+
+var swipers = new Swiper(".slider-content", {
+    slidesPerView: 3,
+    spaceBetween: 60,
+    loop: true,
+    autoplay: true,
+    fade: true,
+    centeredSlide: true,
+    loopFillGroupWithBlank: true,
+    autoplay: {
+        delay: 5000,
+
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
